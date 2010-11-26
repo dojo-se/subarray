54261ec8184378f410f28605135c52a733f0c607
Rafael Mendonça França <rafaelmfranca@gmail.com>
2010-11-26 01:34:03 -0300

    Problema não foi resolvido. Commitado código ainda não passando

--- subarray.rb
+++ subarray.rb
@@ -1,5 +1,17 @@
 class MaxSubarray
   def self.resolve(array)
-    [1]
+    max = -1_000_000_000
+    soma= 0
+    a = []
+    for i in array
+      if i > max
+        max = i 
+        
+      end
+    array.each_with_index do |value, index|
+    
+    end
+    end
+    a << max
   end
 end
--- subarray_spec.rb
+++ subarray_spec.rb
@@ -5,4 +5,16 @@ describe MaxSubarray do
   it "Quando tiver apenas um elemento o resultado deve ser ele mesmo" do
     MaxSubarray.resolve([1]).should == [1]
   end
+  
+  it "Quando tiver dois numero positivos retornar o maior deles" do
+    MaxSubarray.resolve([1, 2]).should == [2] 
+  end
+  it "Quando tiver dois números negativos retornar o maior deles" do
+    MaxSubarray.resolve([-1, -2]).should == [-1]
+  end
+  it "Quando tiver 3 posiçoes somar a primeira com a segunda e somar a segunda com a terceira e retornar a maior dos 2" do
+    MaxSubarray.resolve([3,1,-1]).should == [3, 1]
+    # positivo ou nulo
+  end 
+   
 end
